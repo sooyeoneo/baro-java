62611a5cd671a1bdf5f7ca9f21b921ca0fda4a5e
sooyeon_heo <suzi97@naver.com>
2025-05-13 01:46:08 +0900

    first commit

---
+++ src/main/java/com/example/barojava/BaroJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.barojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaroJavaApplication.class, args);
+    }
+
+}
